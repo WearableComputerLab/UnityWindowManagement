eb11835c9f8539ebdf1be0d69484c02e12f8b61c
Shivam Verma <versy016@mymail.unisa.edu.au>
2023-05-09 14:11:23 +0930

    added scripts to mirror changes made on the projected windows application inside unity to the actual windows application

---
+++ Assets/Scripts/MirrorChange.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+public class MirrorChange : MonoBehaviour
+{
+    void Update()
+    {
+        // On every frame, check if the left mouse button was pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Create a ray from the camera through the point where the mouse cursor is
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If the ray hits a collider (which should be your window texture), then...
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Get the point on the texture where the mouse cursor is, in local (texture) coordinates
+                Vector2 localPoint = hit.textureCoord;
+
+                // Get the dimensions of the foreground window
+                Rect rect = GetWindowRect(WinAPI.GetForegroundWindow());
+
+                // Create a point in client coordinates that corresponds to the point on the texture that was clicked
+                WinAPI.POINT point;
+                point.X = (int)(localPoint.x * rect.width);
+                point.Y = (int)(localPoint.y * rect.height);
+
+                // Convert the point from Unity's coordinate system to the Windows screen coordinate system
+                WinAPI.ScreenToClient(WinAPI.GetForegroundWindow(), ref point);
+
+                // Send the click event to the window
+                // The 'lParam' for these messages is the coordinates of the cursor, packed into a single IntPtr
+                WinAPI.SendMessage(WinAPI.GetForegroundWindow(), WinAPI.WM_LBUTTONDOWN, IntPtr.Zero, new IntPtr(point.Y * 0x10000 + point.X));
+                WinAPI.SendMessage(WinAPI.GetForegroundWindow(), WinAPI.WM_LBUTTONUP, IntPtr.Zero, new IntPtr(point.Y * 0x10000 + point.X));
+            }
+        }
+    }
+
+    // Helper method to get the dimensions of a window as a Rect
+    private Rect GetWindowRect(IntPtr window)
+    {
+        WinAPI.RECT rect;
+        WinAPI.GetWindowRect(window, out rect);
+
+        return new Rect(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+    }
+}
---
+++ Assets/Scripts/WinAPI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public static class WinAPI
+{
+    // Function to get the handle for the foreground window
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+
+    // Function to get the dimensions of a window given its handle
+    [DllImport("user32.dll")]
+    public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+    // Function to convert screen coordinates to client coordinates for a specific window
+    [DllImport("user32.dll")]
+    public static extern bool ScreenToClient(IntPtr hWnd, ref POINT lpPoint);
+
+    // Function to send a Windows message to a window (used here for sending click messages)
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+    // Windows message codes for left button down and up, representing a click
+    public const UInt32 WM_LBUTTONDOWN = 0x0201;
+    public const UInt32 WM_LBUTTONUP = 0x0202;
+
+    // Structs representing a point (for coordinates) and a rectangle (for window dimensions)
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+}
